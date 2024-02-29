a8b77fe5756a2b51c8be52ae39b312ada07d9afc
ckocamaz <caferkocamaz14@gmail.com>
2024-02-29 10:17:48 +0300

    finished, need to add include directories

--- Source/src/main.cpp
+++ Source/src/main.cpp
@@ -4,13 +4,32 @@
 
 int main(){
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
-    if (window == NULL)
-    {
-        std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
+	glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Pencere", NULL, NULL);
+	if (window == NULL)
+	{
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	while(!glfwWindowShouldClose(window))
+	{
+		
+		glfwPollEvents();    
+	}
+
+	glfwTerminate();
     return 0;
 }
